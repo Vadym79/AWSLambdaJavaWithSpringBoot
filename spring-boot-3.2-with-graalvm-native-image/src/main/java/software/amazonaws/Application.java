35bf0a8997cd89bfbc07f8e333758b9352d7997b
Vadym Kazulkin <v.kazulkin@iplabs.de>
2024-02-06 17:52:28 +0100

    use record as product

--- spring-boot-3.2-with-graalvm-native-image/src/main/java/software/amazonaws/Application.java
+++ spring-boot-3.2-with-graalvm-native-image/src/main/java/software/amazonaws/Application.java
@@ -1,4 +1,6 @@
 package software.amazonaws;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,8 +9,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
+	  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
     public static void main(String[] args) {
+    	
+    	logger.info("started SpringBoot Application");
         SpringApplication.run(Application.class, args);
+        logger.info("finished loading SpringBoot Application");
     }
     
    
--- spring-boot-3.2-with-graalvm-native-image/src/main/java/software/amazonaws/example/product/entity/Products.java
+++ spring-boot-3.2-with-graalvm-native-image/src/main/java/software/amazonaws/example/product/entity/Products.java
@@ -3,29 +3,8 @@
 
 package software.amazonaws.example.product.entity;
 
-
-
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class Products {
-  private List<Product> products;
-
-  public Products() {
-  }
-
-  public Products(List<Product> products) {
-    this.products = products;
-  }
-
-  public List<Product> getProducts() {
-    return products;
-  }
-
-  public void setProducts(List<Product> products) {
-    this.products = products;
-  }
+public record Products(List<Product> products) {
 }
 
