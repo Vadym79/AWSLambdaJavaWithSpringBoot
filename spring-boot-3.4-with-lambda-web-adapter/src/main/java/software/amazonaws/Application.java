5dbac546081a981686952788c933d4a7778ab355
Vadym Kazulkin <v.kazulkin@iplabs.de>
2024-12-19 19:54:06 +0100

    added spring boot 3.4 lambda web adapter impl

---
+++ spring-boot-3.4-with-lambda-web-adapter/src/main/java/software/amazonaws/Application.java
@@ -0,0 +1,42 @@
+package software.amazonaws;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+import software.amazonaws.example.product.controller.ProductController;
+import software.amazonaws.example.product.dao.DynamoProductDao;
+
+@RestController
+@Import({ ProductController.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class Application {
+
+	/*
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	@Autowired
+	private DynamoProductDao productDao;
+    */
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+
+	/*
+	@PostConstruct
+	public void doPriming() {
+		logger.info("entered do priming method ");
+		productDao.getProduct("0");
+		logger.info("finished do priming method ");
+	}
+	*/
+
+}
---
+++ spring-boot-3.4-with-lambda-web-adapter/src/main/java/software/amazonaws/Priming.java
@@ -0,0 +1,37 @@
+package software.amazonaws;
+
+import org.crac.Context;
+import org.crac.Core;
+import org.crac.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazonaws.example.product.dao.DynamoProductDao;
+
+//@Configuration
+public class Priming implements Resource {
+
+	//@Autowired
+	private DynamoProductDao productDao;
+
+	private static final Logger logger = LoggerFactory.getLogger(Priming.class);
+
+	public Priming() {
+		logger.info("entered constructor for priming ");
+		Core.getGlobalContext().register(this);
+	}
+
+	@Override
+	public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+		logger.info("entered before checkpoint method ");
+		productDao.getProduct("0");
+		logger.info("finished finished before checkpoint method ");
+	}
+
+	@Override
+	public void afterRestore(Context<? extends Resource> context) throws Exception {
+
+	}
+}
