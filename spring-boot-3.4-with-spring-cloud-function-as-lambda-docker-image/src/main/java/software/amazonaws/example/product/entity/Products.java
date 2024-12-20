8a137709c86114b873c7de49efadf9a4fcbc2abf
Vadym Kazulkin <v.kazulkin@iplabs.de>
2024-12-20 11:50:09 +0100

    added sample app for aws lambda as a docker image with spring boot 3.4 and spring cloud function

---
+++ spring-boot-3.4-with-spring-cloud-function-as-lambda-docker-image/src/main/java/software/amazonaws/example/product/entity/Products.java
@@ -0,0 +1,10 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package software.amazonaws.example.product.entity;
+
+import java.util.List;
+
+public record Products(List<Product> products) {
+}
+
---
+++ spring-boot-3.4-with-spring-cloud-function-as-lambda-docker-image/src/main/java/software/amazonaws/example/product/handler/GetProductByIdHandler.java
@@ -0,0 +1,52 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package software.amazonaws.example.product.handler;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import software.amazon.awssdk.http.HttpStatusCode;
+import software.amazonaws.example.product.dao.DynamoProductDao;
+import software.amazonaws.example.product.entity.Product;
+
+@Component
+public class GetProductByIdHandler implements Function<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+	@Autowired
+	private DynamoProductDao productDao;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+
+	private static final Logger logger = LoggerFactory.getLogger(GetProductByIdHandler.class);
+
+	public APIGatewayProxyResponseEvent apply(APIGatewayProxyRequestEvent requestEvent) {
+		String id = requestEvent.getPathParameters().get("id");
+		Optional<Product> optionalProduct = productDao.getProduct(id);
+		try {
+			if (optionalProduct.isEmpty()) {
+				logger.info(" product with id " + id + " not found ");
+				return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.NOT_FOUND)
+						.withBody("Product with id = " + id + " not found");
+			}
+			logger.info(" product " + optionalProduct.get() + " found ");
+			return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.OK)
+					.withBody(objectMapper.writeValueAsString(optionalProduct.get()));
+		} catch (Exception je) {
+			je.printStackTrace();
+			return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.INTERNAL_SERVER_ERROR)
+					.withBody("Internal Server Error :: " + je.getMessage());
+		}
+	}
+}
