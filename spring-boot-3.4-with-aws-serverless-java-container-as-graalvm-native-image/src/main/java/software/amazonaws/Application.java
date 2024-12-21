c58b30c9f48f23fb39961847c0dbc326c6c0f327
Vadym Kazulkin <v.kazulkin@iplabs.de>
2024-12-21 07:26:24 +0100

    added aws lambda as graalvm native image with spring boot 3.4 and aws serverless java container

--- spring-boot-3.4-with-aws-serverless-java-container-as-graalvm-native-image/src/main/java/software/amazonaws/Application.java
+++ spring-boot-3.4-with-aws-serverless-java-container-as-graalvm-native-image/src/main/java/software/amazonaws/Application.java
@@ -4,18 +4,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import software.amazonaws.example.product.controller.ProductController;
 
 
+@Import({ProductController.class })
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
-
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 	
     public static void main(String[] args) {
-    	
-    	logger.info("started SpringBoot Application");
+    	logger.info("entered main method ");    	
         SpringApplication.run(Application.class, args);
-        logger.info("finished loading SpringBoot Application");
-    }   
-
+        logger.info("finished main method "); 
+    }
 }
